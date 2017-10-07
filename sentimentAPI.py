7aa568f8b57b9fa669aecc4ffbf803245d605b36
brkozik <briankozik@gmail.com>
2017-10-07 15:25:31 -0700

    updating sentimentAPI

--- sentimentAPI.py
+++ sentimentAPI.py
@@ -1,4 +1,4 @@
-
+#dependencies needed: tweepy, google cloud platform, haxor, gsearch, termcolor
 import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -7,6 +7,18 @@ from tweepy.streaming import StreamListener
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+
+from hackernews import HackerNews
+hn = HackerNews()
+
+from gsearch.googlesearch import search
+
+import os
+
+import termcolor
+from termcolor import colored
+
+from sentimentnews import news
 
 consumer_key = 'MGDSQ8j3dQYboOittNFGrp4lo'
 consumer_secret = 'bVv3gkh3d2PbCbnrQ65DXhhy2uH3XT0c1VXJ29MEuiwDUm27sl'
@@ -20,6 +32,12 @@ tApi = tweepy.API(auth)
 def print_result(annotations):
     score = annotations.document_sentiment.score
     
+    if score > .3:
+        print(colored('Overall Sentiment: score of {} '.format(score), 'green'))
+        return 0
+    if score < -.3:
+        print(colored('Overall Sentiment: score of {} '.format(score), 'red'))
+        return 0
     print('Overall Sentiment: score of {} '.format(score))
     return 0
 
@@ -38,8 +56,18 @@ def analyze(movie_review_filename):
     # Print the results
     print_result(annotations)
 
-num = input('Type 1 to sample Twitter, 2 for Google News, or 3 for Reddit')
+num = input('Type 0 to debug, Type 1 to sample Twitter, 2 for Hacker News, or 3 for Google Search')
 num = int(num)
+
+if num == 0:
+    try:
+        f = open("data.txt", 'w')
+        f.write("Awesome spectacular amazing incredible I love life")
+    finally:
+        f.close()
+    analyze('data.txt')
+    os.remove('data.txt')
+
 
 if num == 1:
     try:
@@ -49,11 +77,34 @@ if num == 1:
         tweets = tApi.home_timeline()
         for tweet in tweets:
             f.write(tweet.text)
-            print(tweet.text)
+    #print(tweet.text)
     finally:
         f.close()
     analyze('data.txt')
+    os.remove('data.txt')
 
+if num == 2:
+    try:
+        f = open("data.txt", 'w')
+        for story_id in hn.top_stories(limit=10):
+            f.write(str(hn.get_item(story_id)))
+    finally:
+        f.close()
+    analyze('data.txt')
+    os.remove('data.txt')
+
+if num == 3:
+    searchTerm = input('What would you like to search?')
+    #searchTerm = str(searchTerm)
+    try:
+        f = open("data.txt", 'w')
+        results = search(searchTerm)
+        f.write((str(results)))
+#print(str(results))
+    finally:
+        f.close()
+    analyze('data.txt')
+    os.remove('data.txt')
 
 
 
